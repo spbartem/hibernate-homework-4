9ff17db62724120aa6f403c19339d5cfda1aa2ce
spbartem <spbartemm@gmail.com>
2024-12-23 23:42:01 +0300

    first commit

---
+++ src/main/java/ru/productstar/repository/ContactRepository.java
@@ -0,0 +1,29 @@
+package ru.productstar.repository;
+
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import ru.productstar.dao.entity.Contact;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+
+    Contact save(Contact contact);
+    Optional<Contact> findById(long contactId);
+    void deleteById(long contactId);
+    List<Contact> findAll();
+
+    @Transactional
+    @Modifying
+    @Query("update Contact c set c.phone = :phone where c.id = :contactId")
+    void updatePhone(@Param("contactId") long contactId, @Param("phone") String phoneNumber);
+
+    @Transactional
+    @Modifying
+    @Query("update Contact c set c.email = :email where c.id = :contactId")
+    void updateEmail(@Param("contactId") long contactId, @Param("email") String email);
+}
---
+++ src/main/java/ru/productstar/service/ContactService.java
@@ -0,0 +1,7 @@
+package ru.productstar.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContactService {
+}
